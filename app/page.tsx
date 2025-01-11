d9cfe6224695daa16bea08bc40fde7bcc1171783
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-01-11 16:20:59 +0530

    background

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-"use client";
 import React from "react";
-import { BackgroundBeams } from "../app/components/ui/background-beams";
 
-export default function BackgroundBeamsDemo() {
-  return (
-      <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-        <BackgroundBeams />
-      </div>
-  );
+export default function GridBackgroundDemo() {
+    return (
+        <div className="h-[50rem] w-full bg-black bg-grid-white/[0.2] relative flex items-center justify-center">
+            {/* Radial gradient for the container to give a faded look */}
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+            <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+                Backgrounds
+            </p>
+        </div>
+    );
 }
