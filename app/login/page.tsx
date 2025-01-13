db8f514a55a89d3b8772e8d8d43a57a631c638a8
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-01-14 03:34:19 +0530

    road to auth

---
+++ app/login/page.tsx
@@ -0,0 +1,62 @@
+// // import React from "react";
+// import * as React from "react"
+//
+// import { Button } from "@/components/ui/button"
+// import {
+//     Card,
+//     CardContent,
+//     CardFooter,
+//     CardHeader,
+//     CardTitle,
+// } from "@/app/components/ui/card"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
+//
+//
+// export default function GridBackgroundDemo() {
+//     return (
+//         <div className="h-[50rem] w-full bg-black bg-grid-white/[0.2] relative flex items-center justify-center">
+//             {/* Radial gradient for the container to give a faded look */}
+//             <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+//             <Card className="w-[350px]">
+//                 <CardHeader>
+//                     <CardTitle>User Login </CardTitle>
+//                     {/*<CardDescription>Deploy your new project in one-click.</CardDescription>*/}
+//                 </CardHeader>
+//                 <CardContent>
+//                     <form>
+//                         <div className="grid w-full items-center gap-4">
+//                             <div className="flex flex-col space-y-1.5">
+//                                 <Label htmlFor="name">Name</Label>
+//                                 <Input id="name" placeholder="Email" />
+//                             </div>
+//                             <div className="flex flex-col space-y-1.5">
+//                                 <Label htmlFor="framework">Password</Label>
+//                                 <Input id="name" placeholder="Password" />
+//                             </div>
+//                         </div>
+//                     </form>
+//                 </CardContent>
+//                 <CardFooter className="flex justify-between">
+//                     <Button variant="outline">Login</Button>
+//                     <Button>Sign Up</Button>
+//                 </CardFooter>
+//             </Card>
+//         </div>
+//     );
+// }
+
+import { login, signup } from './actions'
+
+export default function LoginPage() {
+    return (
+        <form>
+            <label htmlFor="email">Email:</label>
+            <input id="email" name="email" type="email" required />
+            <label htmlFor="password">Password:</label>
+            <input id="password" name="password" type="password" required />
+            <button formAction={login}>Log in</button>
+            <button formAction={signup}>Sign up</button>
+        </form>
+    )
+}
