b8120d42c001508780f8eb766984a525fcfbcaa1
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-03-15 19:54:42 +0530

    deployment -7

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,7 +7,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Textarea } from "@/app/components/ui/textarea"
 import {UserButton} from "@clerk/nextjs";
 
 import { useUser } from '@clerk/nextjs';
